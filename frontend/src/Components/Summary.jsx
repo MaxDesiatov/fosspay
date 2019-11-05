0d39c467039ba659c9e3e88cd65cb489ba287293
Max Desiatov <max@desiatov.com>
2019-11-05 22:37:43 +0000

    Use relative links in Summary, improve panel UI

--- frontend/src/Components/Summary.jsx
+++ frontend/src/Components/Summary.jsx
@@ -5,12 +5,12 @@ export default () => (
   <Body>
     Your contributions allow me to spend more time on{' '}
     <a href='https://github.com/MaxDesiatov'>open-source projects</a> and to
-    produce more content for <a href='https://desiatov.com'>my blog</a>. You can
-    also pick a specific project you'd like to support in the form below.
+    produce more content for <a href='/'>my blog</a>. You can also pick a
+    specific project you'd like to support in the form below.
     <br />
     <br />
     Been here before? Then you can also access{' '}
-    <a href='https://desiatov.com/sponsor/panel'>your sponsorship account</a> to
-    manage your payments.
+    <a href='/sponsor/panel'>your sponsorship account</a> to manage your
+    payments.
   </Body>
 );
